323e1769854e09a00b72a8e857a9720ec30961d3
mjarzebkowski <maciej.jarzebkowski@gmail.com>
2024-06-28 00:45:32 +0200

    rm EntityFrameworkCore

--- Whisper/Whisper.Server/Data/ApplicationUser.cs
+++ Whisper/Whisper.Server/Data/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace Whisper.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Key { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
     }
 
 }
